6c058d75ae29e13d1e86ff1e30c9bc0938bd3149
szq <szq@qq.com>
2023-08-15 21:58:03 +0800

    Initial commit

---
+++ docker-demo01/src/test/java/com/szq/dockerdemo01/DockerDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szq.dockerdemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
